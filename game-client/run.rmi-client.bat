18f041ce37145660bcb6727b99823f16ff52bf34
Oleg <ogretoleg@yandex.ru>
2019-03-26 21:41:37 +0300

    reorg2

--- game-client/run.rmi-client.bat
+++
@@ -1,7 +0,0 @@
-java -version
-
-set CLASSPATH=C:\Users\Oleg\Desktop\ProjectRMI\-distributed-system-RMI\game-client\build\classes
-
-java points.client.PointsClient
-
-PAUSE
--- game-server/run.rmi-server.bat
+++
@@ -1,7 +0,0 @@
-java -version
-
-set CLASSPATH=C:\Users\Oleg\Desktop\ProjectRMI\-distributed-system-RMI\game-server\build\classes
-
-java points.server.PointsServer 4 6
-
-PAUSE
